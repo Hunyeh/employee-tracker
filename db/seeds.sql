c806cbd94dc801f8b3f2fbe890b53e583d79a6f6
Hunter <88465412+Hunyeh@users.noreply.github.com>
2021-11-08 20:55:03 -0500

    populated some info in seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,3 +5,22 @@ VALUES ('Finance'),
        ('Sales'),
        ('Engineering'),
        ('Legal');
+
+INSERT INTO role (title, salary, department_id)
+VALUES ('Sales Lead', '100000', ''),
+       ('Salesperson', '80000', ''),
+       ('Lead Engineer', '150000', ''),
+       ('Software Engineer', '120000', ''),
+       ('Accountant', '125000', ''),
+       ('Legal Team Lead', '250000', ''),
+       ('Lawyer', '190000', '');
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+Values ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', ''),
+       ('', '', '', '');
